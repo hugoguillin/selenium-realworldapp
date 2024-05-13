be72176a4beaa8cf3edfe11a3733ff5b87725b03
hugoguillin <hugo5guillin@gmail.com>
2024-05-13 23:45:37 +0200

    First commit

---
+++ src/test/java/com/realworld/seleniumrealworldapp/SeleniumRealworldappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realworld.seleniumrealworldapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumRealworldappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
