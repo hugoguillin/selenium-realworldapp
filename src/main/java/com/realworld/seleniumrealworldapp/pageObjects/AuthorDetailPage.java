598c535954ef8ef45e41af555e868e34634f6f9e
hugoguillin <hugo5guillin@gmail.com>
2024-05-21 00:03:09 +0200

    Add second test

--- src/main/java/com/realworld/seleniumrealworldapp/pageObjects/AuthorDetailPage.java
+++ src/main/java/com/realworld/seleniumrealworldapp/pageObjects/AuthorDetailPage.java
@@ -1,10 +1,18 @@
 package com.realworld.seleniumrealworldapp.pageObjects;
 
+import com.google.common.net.MediaType;
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.jayway.jsonpath.JsonPath;
 import com.realworld.seleniumrealworldapp.infra.annotations.PageObject;
 import com.realworld.seleniumrealworldapp.utils.api.ArticlesApi;
+import org.openqa.selenium.devtools.NetworkInterceptor;
+import org.openqa.selenium.remote.http.Contents;
+import org.openqa.selenium.remote.http.HttpResponse;
+import org.openqa.selenium.remote.http.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
+import java.util.concurrent.TimeUnit;
 
 
 @PageObject
@@ -20,5 +28,21 @@ public class AuthorDetailPage extends BasePage {
         driver.get(baseUrl + "/profile/" + authorName);
         wait.until(driver -> getElementByText("My Articles").getAttribute("class").contains("active"));
         return authorName;
+    }
+
+    /**
+     * Mocks the API response to display the favorited articles
+     */
+    public void goToFavoritedArticles(Object file) {
+        try (NetworkInterceptor ignored = new NetworkInterceptor(
+                driver, Route.matching(req -> req.getUri().matches(".*articles\\?favorited=.*"))
+                .to(() -> req ->
+                        new HttpResponse()
+                                .setStatus(200)
+                                .addHeader("Content-Type", MediaType.JSON_UTF_8.toString())
+                                .setContent(Contents.asJson(file))))) {
+            getElementByText("Favorited Articles").click();
+            Uninterruptibles.sleepUninterruptibly(200, TimeUnit.MILLISECONDS);
+        }
     }
 }
--- src/test/java/com/realworld/seleniumrealworldapp/AuthorDetailTests.java
+++ src/test/java/com/realworld/seleniumrealworldapp/AuthorDetailTests.java
@@ -1,16 +1,21 @@
 package com.realworld.seleniumrealworldapp;
 
 import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.ReadContext;
 import com.realworld.seleniumrealworldapp.base.BaseTest;
 import com.realworld.seleniumrealworldapp.pageObjects.AuthorDetailPage;
 import com.realworld.seleniumrealworldapp.pageObjects.components.ArticlesFeedPage;
 import com.realworld.seleniumrealworldapp.utils.api.AuthorApi;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.ResourceUtils;
 
+import java.io.IOException;
 import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -26,6 +31,7 @@ public class AuthorDetailTests extends BaseTest {
     @Test
     @Tag("articles")
     @Tag("author")
+    @DisplayName("Should display author articles")
     public void displayAuthorArticles() {
         // Arrange
         var authorName = authorDetailPage.visit();
@@ -35,4 +41,24 @@ public class AuthorDetailTests extends BaseTest {
         // Assert
         assertThat(articlesDisplayed).containsExactlyInAnyOrderElementsOf(authorArticles);
     }
+
+    @Test
+    @Tag("articles")
+    @Tag("author")
+    @DisplayName("Should display favorited articles")
+    public void displayFavoritedArticles() throws IOException {
+        // Arrange
+        authorDetailPage.visit();
+        ReadContext ctx = JsonPath.parse(ResourceUtils.getFile("classpath:mockedFavoritedArticles.json"));
+        var mockedFavoritedArticles = ctx.read("$");
+        List<String> articlesTitles = ctx.read("$.articles[*].title");
+        int numberOfArticles = ctx.read("$.articles.length()");
+        authorDetailPage.goToFavoritedArticles(mockedFavoritedArticles);
+        var articlesDisplayed = articlesFeedPage.getArticleTitles();
+
+        // Assert
+        assertThat(articlesDisplayed.size()).isEqualTo(numberOfArticles);
+        assertThat(articlesDisplayed).containsExactlyInAnyOrderElementsOf(articlesTitles);
+    }
+
 }
