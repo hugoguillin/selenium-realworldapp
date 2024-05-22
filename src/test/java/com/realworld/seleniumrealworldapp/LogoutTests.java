ed21472239e8676f9bf6e73dd71cee01ce054527
hugoguillin <hugo5guillin@gmail.com>
2024-05-22 23:30:42 +0200

    Add first test

---
+++ src/test/java/com/realworld/seleniumrealworldapp/LogoutTests.java
@@ -0,0 +1,33 @@
+package com.realworld.seleniumrealworldapp;
+
+import com.realworld.seleniumrealworldapp.base.BaseTest;
+import com.realworld.seleniumrealworldapp.pageObjects.UserSettingsPage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class LogoutTests extends BaseTest {
+    @Value("${base.url}")
+    private String baseUrl;
+    @Autowired
+    private UserSettingsPage userSettingsPage;
+
+    @Test
+    @Tag("user")
+    @DisplayName("Should navigate to home page after logout")
+    public void logout() {
+        // Arrange
+        userSettingsPage.visit();
+        userSettingsPage.performLogout();
+
+        // Assert
+        assertThat(driver.getCurrentUrl()).isEqualTo(baseUrl + "/");
+        assertThat(driver.findElements(userSettingsPage.getUserPicLocator())).isEmpty();
+    }
+}
