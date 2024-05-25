86cb7703c3468b18ccc8f590071c3c1c30afab8e
hugoguillin <hugo5guillin@gmail.com>
2024-05-25 20:17:54 +0200

    Add second test

--- src/test/java/com/realworld/seleniumrealworldapp/SignUpTests.java
+++ src/test/java/com/realworld/seleniumrealworldapp/SignUpTests.java
@@ -5,18 +5,22 @@ import com.realworld.seleniumrealworldapp.base.BaseTest;
 import com.realworld.seleniumrealworldapp.infra.NetworkInterceptor;
 import com.realworld.seleniumrealworldapp.pageObjects.SignUpPage;
 import com.realworld.seleniumrealworldapp.utils.Utils;
+import com.realworld.seleniumrealworldapp.utils.entities.NewUser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.JavascriptExecutor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class SignUpTests extends BaseTest {
+    @Value("${user.email}")
+    private String testUserEmail;
     @Autowired
     private SignUpPage signUpPage;
     @Autowired
@@ -45,5 +49,20 @@ public class SignUpTests extends BaseTest {
         // Assert
         assertThat(registeredEmail).isEqualTo(newUser.email());
         assertThat(signUpPage.getUsernameFromTopBar()).isEqualTo(newUser.username());
+    }
+
+    @Test
+    @Tag("user")
+    @DisplayName("Should display error message when registering with existing email")
+    public void registerWithExistingEmail() {
+        // Arrange
+        var newUser = new NewUser("test", testUserEmail, "test1234");
+        signUpPage.visit();
+
+        // Act
+        signUpPage.registerNewUser(newUser);
+
+        // Assert
+        signUpPage.assertErrorMessageIsVisible("Email already exists.. try logging in");
     }
 }
