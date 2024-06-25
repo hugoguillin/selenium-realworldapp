41156b81b79dfbc1f80675373788e91f412ce4da
hugoguillin <hugo5guillin@gmail.com>
2024-06-25 17:29:41 +0200

    Test workflow

--- src/test/java/com/realworld/seleniumrealworldapp/SeleniumRealworldappApplicationTests.java
+++ src/test/java/com/realworld/seleniumrealworldapp/SeleniumRealworldappApplicationTests.java
@@ -1,7 +1,12 @@
 package com.realworld.seleniumrealworldapp;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
 
 @SpringBootTest
 class SeleniumRealworldappApplicationTests {
+    @Test
+    void test() {
+        System.out.println("Let's download dependencies!");
+    }
 }
